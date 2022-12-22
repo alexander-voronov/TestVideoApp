d4745e226ca4a3ee212052c6906f1827bf43d848
Alexander Voronov <dr.strum@gmail.com>
2022-12-22 18:17:00 +0300

    Added test VideoFragment.kt

---
+++ app/src/main/java/alexander/voronov/testvideoapp/ui/PreviewViewModel.kt
@@ -0,0 +1,7 @@
+package alexander.voronov.testvideoapp.ui
+
+import alexander.voronov.testvideoapp.domain.VideoRepository
+import androidx.lifecycle.ViewModel
+
+class PreviewViewModel(val repository: VideoRepository): ViewModel() {
+}
--- app/src/main/java/alexander/voronov/testvideoapp/ui/VideoFragment.kt
+++ app/src/main/java/alexander/voronov/testvideoapp/ui/VideoFragment.kt
@@ -1,14 +1,15 @@
 package alexander.voronov.testvideoapp.ui
 
 import alexander.voronov.testvideoapp.databinding.FragmentVideoBinding
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class VideoFragment : Fragment() {
 
+class VideoFragment : Fragment() {
     private var _binding: FragmentVideoBinding? = null
     private val binding get() = _binding!!
 
@@ -19,6 +20,18 @@ class VideoFragment : Fragment() {
     ): View {
         _binding = FragmentVideoBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val videoSource =
+            "https://storage.googleapis.com/assets-stage-bgrem-deelvin-com/bg/videos/051d0387-3098-4b1a-81c4-bcd8f5516cc4/preview-u5z128qp.mp4"
+        binding.viewVideo.setVideoURI(Uri.parse(videoSource))
+
+        binding.viewVideo.setOnPreparedListener { mediaPlayer ->
+            mediaPlayer.isLooping = true
+        }
+        binding.viewVideo.start()
     }
 
     override fun onDestroy() {
